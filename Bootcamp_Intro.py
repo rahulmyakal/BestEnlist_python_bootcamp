4460eb64e04bac030d010b36801730e272d1dc8f
Rahul Chandan Myakal <74832006+rahulmyakal@users.noreply.github.com>
2021-06-12 18:46:00 +0530

    intro to bootcamp

---
+++ Bootcamp_Intro.py
@@ -0,0 +1,9 @@
+''' ______________________ Basic  of Python _________________________'''
+
+str="BEST ENLIST IS PROVINDING PYTHON INTERNSHIP\BOOTCAMP WITH FREE OF COST"
+print('Length of String is : ',len(str))
+print('string in lower case :\t',str.lower())
+
+a,b,c,d=24,64,74,22
+print('Max Number from above variable is : ',max(a,b,c,d))
+print('Addition of two number is : ',a+b+c+d)
